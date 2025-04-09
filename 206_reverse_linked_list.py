aabeb672e28fe4cbede99efeb387750f9eddf7bf
Greg057 <88341092+Greg057@users.noreply.github.com>
2025-04-09 14:50:56 +1000

    206

---
+++ 206_reverse_linked_list.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# Iterative approach
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        cur = head
+
+        while cur:
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+        return prev
+    
+# Recursive approach
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return self.recursiveReverse(None, head)
+
+    def recursiveReverse(self, prev, cur):
+        if cur is None:
+            return prev
+        else:
+            next = cur.next
+            cur.next = prev
+            return self.recursiveReverse(cur, next)
