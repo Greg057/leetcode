79996a1697cd15449dbdca0ae9218a4e97adf8cb
Greg057 <88341092+Greg057@users.noreply.github.com>
2025-04-09 16:44:09 +1000

    add 226 + fix imports

--- 141.py
+++ 141.py
@@ -1,10 +1,10 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-from typing import Optional, ListNode
+from typing import Optional
 
 
 # A set is used instead of a list or dict because it provides O(1) 
--- 21.py
+++ 21.py
@@ -1,10 +1,10 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-from typing import Optional, ListNode
+from typing import Optional
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
