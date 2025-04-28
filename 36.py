4a7bd2808b363a264a1c9c63b9385339c4bb9c42
Greg057 <88341092+Greg057@users.noreply.github.com>
2025-04-28 15:42:49 +1000

    add 36

---
+++ 36.py
@@ -0,0 +1,52 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = {}
+        rows = {}
+        squares = {}
+
+        for i in range(9):
+            cols[i] = set()
+            rows[i] = set()
+        
+        for j in range(3):
+            for k in range(3):
+                squares[(j, k)] = set()
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    continue
+                if (board[r][c] in rows[r] or 
+                   board[r][c] in cols[c] or
+                   board[r][c] in squares[(r//3, c//3)]):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
+        return True           
+
+# Alternative solution using defaultdict
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)  
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if ( board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]):
+                    return False
+
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+
+        return True
