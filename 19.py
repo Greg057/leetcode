302694fae407b2be0257bbfa3261bc762667f5bf
Greg057 <88341092+Greg057@users.noreply.github.com>
2025-05-16 16:22:06 +1000

    add optimal solution 19

--- 19.py
+++ 19.py
@@ -5,6 +5,27 @@ class ListNode:
         self.next = next
 
 from typing import Optional
+
+# one Pass
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+
+        while n > 0:
+            right = right.next
+            n -= 1
+
+        while right:
+            left = left.next
+            right = right.next
+
+        left.next = left.next.next
+        return dummy.next
+    
+
+# two Pass
 # time complexity: O(n)
 # space complexity: O(1) for constant space
 # The algorithm uses O(n) time to find the length of the list and then O(n) time to remove the nth node from the end.
